52919180dbd08ee13d9a0a33bab58f7ef0249a3c
Emily S <emily.sugano@gmail.com>
2018-07-31 17:22:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,6 +12,7 @@ class PigLatinizer
         elsif 'aeiou'.include? word[0].downcase
           "#{word}way"
         else
+          binding.pry
           new_word = word.split(/([aeiou])/,2)
           "#{new_word[1]}#{new_word[2]}#{new_word[0]}ay"
         end
