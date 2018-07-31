1bd187fcdf525135ad60eb86df1f9839741f59b1
Emily S <emily.sugano@gmail.com>
2018-07-31 17:11:06 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,17 +1,19 @@
 require 'pry'
 
 class PigLatinizer
-  attr_accessor :phrase, :pl_word
+  attr_accessor :phrase
 
     def piglatinize(phrase)
-      @phrase = phrase.split(" ")
-      @pl_word = []
-
-      @phrase.each do |word|
-        @pl_word << word.scan(/[aeoui]/)
-        @pl_word  << word.scan(/[bcdfghjklmnpqrstvwxyz]/)
-        @pl_word << "ay"
-      end
-      @pl_word.flatten.join
+      @phrase = phrase.downcase.split(" ")
+      @phrase.map do |word|
+        if word.length == 1
+          "#{word}way"
+        elsif vowel.include? word[0].downcase
+          "#{word}way"
+        else 
+          new_word = word.split(/([aeiou])/,2)
+          "#{new_word[1]}#{new_word[2]}#{new_word[0]}ay"
+        end
     end
+    
 end
