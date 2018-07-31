c400361ae40c1d7790b93cd7ed8394d9d52f6e34
Emily S <emily.sugano@gmail.com>
2018-07-31 17:12:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,11 +4,11 @@ class PigLatinizer
   attr_accessor :phrase
 
     def piglatinize(phrase)
-      @phrase = phrase.downcase.split(" ")
+      @phrase = phrase.downcase.split()
       @phrase.map do |word|
         if word.length == 1
           "#{word}way"
-        elsif vowel.include? word[0].downcase
+        elsif 'aeiou'.include? word[0].downcase
           "#{word}way"
         else
           new_word = word.split(/([aeiou])/,2)
