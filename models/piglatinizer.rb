270938a455dc91599934133828e75b4f34ebf132
Emily S <emily.sugano@gmail.com>
2018-07-31 16:36:01 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,13 +5,14 @@ class PigLatinizer
       @phrase = phrase.split(" ")
       @vowels = []
       @constants = []
-      @pl_word = 
+      @pl_word = []
 
       phrase.each do |word|
         # @vowels = @word.scan(/[aeoui]/)
         # @constants = @word.scan(/[bcdfghjklmnpqrstvwxyz]/)
-        @pl_word << @word.scan(/[aeoui]/)
-        @pl_word  << @word.join.scan(/[bcdfghjklmnpqrstvwxyz]/)
+        @pl_word << word.scan(/[aeoui]/)
+        @pl_word  << word.join.scan(/[bcdfghjklmnpqrstvwxyz]/)
         @pl_word
+        binding.pry
       end
 end
